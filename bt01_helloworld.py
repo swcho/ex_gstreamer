94ea2dc399989722276d8501232f7e5c78155c63
sungwoo <swforgemaster@gmail.com>
2014-07-04 15:19:59 +0900

    Initial commit

---
+++ bt01_helloworld.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+__author__ = 'sungwoo'
+
+# http://docs.gstreamer.com/pages/viewpage.action?pageId=327735
+
+import gst
+import gtk
+
+pipeline = gst.parse_launch("playbin2 uri=http://docs.gstreamer.com/media/sintel_trailer-480p.webm")
+pipeline.set_state(gst.STATE_PLAYING)
+bus = pipeline.get_bus()
+bus.timed_pop_filtered(gst.CLOCK_TIME_NONE, gst.MESSAGE_ERROR | gst.MESSAGE_EOS)
+# gtk.main()
